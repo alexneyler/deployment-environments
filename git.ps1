9420b1afd6e2cb7cb29ee1b52637a458ceec6908
Alex Eyler <alexey@microsoft.com>
2025-03-06 13:48:32 -0800

    Add git config set up

---
+++ git.ps1
@@ -0,0 +1,8 @@
+git config --global user.name 'Alex Eyler'
+git config --global user.email 'alexey@microsoft.com'
+git config --global push.autoSetupRemote true
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.last 'log -1 HEAD'
